061ccbb2519f92d6a56e379015fa2416dc0d7bfc
noistommy <hioran@nate.com>
2025-06-23 14:25:32 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const pkg = require('gulp');
+const {series, parallel, src, dest, watch} = pkg;
+const sassGlob = require('gulp-sass-glob');
+const sass = require('gulp-dart-sass');  // sass/scss -> css 빌드
+const postcss = require('gulp-postcss');
+const minifyCSS = require('gulp-clean-css'); // 최소화
+const uglify = require('gulp-uglify'); // 난독화
+const { deleteAsync } = require('del');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+
+const pug = require('gulp-pug');
+
+const readlineSync = require('readline-sync');
+
+const fs = require('fs');
+
+const prompt = require('gulp-prompt');
+
+const log = require('fancy-log');
+
+// fs module test
+// fa.writeFile('sample.json', JSON.stringify({ sample: 'data' }), () => {})
+
+let paths = {
+  paths: {
+    output: {
+      packaged: 'dist'
+    }
+  }
+}
+
+fs.readFile('config.json', 'utf-8', (err, data) => {
+  if (err) return console.log(err)
+  paths = JSON.parse(data)
+  log(paths)
+})
+
+// 빌드 전 이전 빌드 폴더 삭제
+const clean = () => {
+  // site();
+  return deleteAsync(['dist/'])
+};
+
+// scss 빌드
+const build = () => {
+  const baseFile = 'frogui'
+  log(baseFile, paths.base)
+  return src([`src/${baseFile}.scss`])
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('./dist', {sourcemaps:false}))
+    .pipe(rename(`${baseFile}.css`))
+    .pipe(minifyCSS())
+    .pipe(dest('./site/public'))
+}
+// JS 빌드
+const buildJS = () => {
+  return src(['src/definitions/components/*.js', 'example/base.js'])
+    // .pipe(concat('frog_ui.js'))
+    .pipe(dest('./dist/js'))
+    .pipe(uglify())
+    .pipe(dest('./site/public/js'))
+}
+
+// assets 파일 빌드 폴더로 가져가기
+const assets = () => {
+  return src('src/assets/**')
+    .pipe(dest('./example/build/assets'))
+}
+//
+
+// 작업 중 변경 감지 -> 빌드
+const watcher = () => {
+  watch(['src/themes/default/**/*.scss','src/definitions/**/**/*.scss'], build)
+  watch(['src/themes/default/bases/variables.scss', 'src/themes/default/bases/reset.scss'], build)
+  watch('src/definitions/components/*.js', buildJS)
+
+  watch(['example/*.scss'], site)
+  watch(['example/**/*.pug'], viewDev)
+}
+
+// 예제 사이트 watch & build
+const siteWatcher = () => {
+  watch(['example/style.scss'], site)
+  watch(['example/**/*.pug'], viewDev)
+}
+// 에제 페이지 컴파일
+const viewDev = () => {
+  return src(['example/index.pug', 'example/**/*.pug'])
+    .pipe(pug())
+    .pipe(dest('./demo',{sourcemaps:false}))
+}
+
+const site = () => {
+  return src(['example/style.scss'])
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(concat('style.css'))
+    .pipe(minifyCSS())
+    .pipe(dest('./demo'))
+}
+
+// install 여부 확인 (테스트 중...)
+const checkBuild = async (done) => {
+  if(readlineSync.keyInYN('Do you want to build?')) {
+    return await done()
+  }
+  log('OK. Not building')
+}
+
+const question = () => {
+  return src('gulpfile.js')
+    .pipe(prompt.prompt({
+        type: 'input',
+        name: 'tesk',
+        message: 'are you OK?'
+    },
+    (res) => {
+        log(res)
+        if(res.tesk === 'yes') {
+            build ()
+        }
+    }))
+}
+// ... existing code ...
+
+module.exports.question = question;
+module.exports.clean = clean;
+module.exports.build = build;
+module.exports.devSite = series(site, siteWatcher);
+module.exports.default = series(clean, parallel(viewDev, site, build, buildJS, assets), watcher, () => {
+    log("Run gulp!!")
+});
---
+++ site/routes/index.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/pages/base', function(req, res, next) {
+  res.render('components/base',  {title: 'Base' });
+});
+
+router.get('/pages/button', function(req, res, next) {
+  res.render('components/button',  { title: 'Button' });
+});
+
+router.get('/pages/badge', function(req, res, next) {
+  res.render('components/badge',  { title: 'Badge' });
+});
+
+router.get('/pages/calendar', function(req, res, next) {
+  res.render('components/calendar',  { title: 'Calendar' });
+});
+
+router.get('/pages/card', function(req, res, next) {
+  res.render('components/card',  { title: 'Card' });
+});
+
+router.get('/pages/checkbox', function(req, res, next) {
+  res.render('components/checkbox',  { title: 'Checkbox' });
+});
+
+router.get('/pages/colors', function(req, res, next) {
+  res.render('components/colors',  { title: 'Colors' });
+});
+
+router.get('/pages/form', function(req, res, next) {
+  res.render('components/form',  { title: 'Form' });
+});
+
+router.get('/pages/grid', function(req, res, next) {
+  res.render('components/grid',  { title: 'Grid' });
+});
+
+router.get('/pages/grid2', function(req, res, next) {
+  res.render('components/grid2',  { title: 'Grid2' });
+});
+
+router.get('/pages/input', function(req, res, next) {
+  res.render('components/input',  { title: 'Input' });
+});
+
+router.get('/pages/list', function(req, res, next) {
+  res.render('components/list',  { title: 'List' });
+});
+
+router.get('/pages/message', function(req, res, next) {
+  res.render('components/message',  { title: 'Message' });
+});
+
+router.get('/pages/modal', function(req, res, next) {
+  res.render('components/modal',  { title: 'Modal' });
+});
+
+router.get('/pages/pagination', function(req, res, next) {
+  res.render('components/pagination',  { title: 'Pagination' });
+});
+
+router.get('/pages/panel', function(req, res, next) {
+  res.render('components/panel',  { title: 'Panel' });
+});
+
+router.get('/pages/selectbox', function(req, res, next) {
+  res.render('components/selectbox',  { title: 'Selectbox' });
+});
+
+router.get('/pages/switch', function(req, res, next) {
+  res.render('components/switch',  { title: 'Switch' });
+});
+
+router.get('/pages/tabs', function(req, res, next) {
+  res.render('components/tabs',  { title: 'Tabs' });
+});
+
+router.get('/pages/tag', function(req, res, next) {
+  res.render('components/tag',  { title: 'Tag' });
+});
+
+router.get('/pages/toast', function(req, res, next) {
+  res.render('components/toast',  { title: 'Toast' });
+});
+
+
+module.exports = router;
