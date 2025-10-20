a51fab7307b8bcc2500de3aaef9d7177c40280c5
noistommy <hioran@nate.com>
2025-10-20 17:37:46 +0900

    add plausible analytics sys

--- site/routes/index.js
+++ site/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Frog' });
+  res.render('index', { title: 'Frog UI' });
 });
 
 router.get('/pages/base', function(req, res, next) {
