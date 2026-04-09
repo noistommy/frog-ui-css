5524ce1c0d66fcde47a38f770823037dfc2b820e
noistommy <hioran@nate.com>
2026-04-09 16:57:05 +0900

    update panels style: accordion

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# ga-ui-css
-ga ui build system
-# frog-ui-css
+# Frog UI
+
+"Semantic CSS를 사용하여 일관되고 의미있게 화면을 구현합니다."
+
+## 개요
+Frog UI는 scss로 작성되고 gulp로 빌드되는 CSS Framework 입니다. 빌드를 통해 하나의 정적(.css)파일로 만들어지며 그렇기 때문에 프레임워크를 제한없이 적용 할 수 있습니다. 
+
+
--- src/definitions/components/panel.js
+++ src/definitions/components/panel.js
@@ -13,3 +13,26 @@ if(panel) {
         }
     })
 }
+
+const accordion = document.querySelector('.ga-accordion');
+
+if(accordion) {
+    const list = accordion.querySelectorAll('.ga-panel');
+
+    const setPanel = (index) => {
+        list.forEach((c, i) => {
+            if(i === index) c.classList.add('open')
+            else c.classList.remove('open')
+        })
+
+    }
+    list.forEach((p, i) => {
+        p.addEventListener('click', () => {
+            if (p.classList.contains('open')) {
+                p.classList.remove('open')
+            } else {
+                setPanel(i)
+            }
+        })
+    })
+}
